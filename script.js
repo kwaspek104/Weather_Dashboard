59c8eb270ced3a595f754808f24b73360bdcfa47
kwaspek104 <kasper.kuzmicki@yahoo.com>
2020-11-29 16:23:24 -0500

    It's not working for me anymore.

--- script.js
+++ script.js
@@ -1,37 +1,31 @@
-// let cityNameEl = $(".cityName");
-// let tempEl = $(".temp");
-// let humidityEl = $(".humidity");
-// let windSpeedEl = $(".windSpeed");
-// let UVIndexEl = $(".UVIndex");
+$(document).ready(function () {
 
-
-$(document).ready(function() {
-
-    $('#submitWeather').click(function(){
+    $('#submitWeather').click(function () {
         var city = $("#city").val();
 
-        
-$.ajax({
-    url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=a1714656806d3c4da922178094aa251a"
-    ,
-    method: "GET",
-    success: function(data) {
-        var widget = show(data);
-        $(".content").html(widget);
-        $("#city").val('');
-    }
-});
-        
-
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=a1714656806d3c4da922178094aa251a",
+            method: "GET",
+            success: function (data) {
+                console.log(data);
+                var widget = show(data);
+                $(".content").html(widget);
+                $("#city").val('');
+            },
+        });
+    
     });
-
-});
 
 function show(data) {
     return "<h1> <img class='image is-48x48' src= https://openweathermap.org/img/w/" + data.weather[0].icon + ".png>" + data.name + "</h1>" +
-    "<p>Temperature: " + data.main.temp + "</p>" +
-    "<p>Humidity: " + data.main.humidity + "</p>" +
-    "<p>Wind Speed: " + data.wind.speed + "</p>"
-    
+        "<p>Temperature: " + data.main.temp + "</p>" +
+        "<p>Humidity: " + data.main.humidity + "</p>" +
+        "<p>Wind Speed: " + data.wind.speed + "</p>"
 }
+
+});
+
+
+    
+   
 
