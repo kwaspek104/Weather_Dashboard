e2155652ae72439e49b29b03d9fc43f5bf27f450
Kasper Kuzmicki <kasper.kuzmicki@yahoo.com>
2020-12-22 15:11:30 -0500

    I'm gonna commit before I do some MAJOR changes.

---
+++ config.js
@@ -0,0 +1,3 @@
+const config = {
+    SECRET_API_KEY : '9c59ac0824aa13bbe8aae57be9271bd0',
+  }
--- script.js
+++ script.js
@@ -1,31 +1,54 @@
 $(document).ready(function () {
 
-    $('#submitWeather').click(function () {
-        var city = $("#city").val();
+    const apiKey = config.SECRET_API_KEY
 
+    $('#submit-weather').on('click', function (event) {
+        event.preventDefault()
+        const city = $("#city").val();
         $.ajax({
-            url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=a1714656806d3c4da922178094aa251a",
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`,
             method: "GET",
             success: function (data) {
-                console.log(data);
-                var widget = show(data);
+                const widget = show(data);
                 $(".content").html(widget);
                 $("#city").val('');
+                getUVIndex(data.coord.lat, data.coord.lon)
             },
         });
-    
     });
 
-function show(data) {
-    return "<h1> <img class='image is-48x48' src= https://openweathermap.org/img/w/" + data.weather[0].icon + ".png>" + data.name + "</h1>" +
-        "<p>Temperature: " + data.main.temp + "</p>" +
-        "<p>Humidity: " + data.main.humidity + "</p>" +
-        "<p>Wind Speed: " + data.wind.speed + "</p>"
-}
+    function getUVIndex(lat, lon) {
+        $.ajax({
+            url: `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`,
+            method: "GET",
+            success: function (data) {
+                const UVIndex = "<p>UV Index: " + data.value + "</p>"
+                $('.content').append(UVIndex)
+            },
+        })
+    };
+
+    // function fiveDayForecast() {
+    //     $.ajax({
+    //         url: `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${apiKey}`,
+    //         method: "GET",
+    //         success: function (data) {
+    //             const UVIndex = "<p>UV Index: " + data.value + "</p>"
+    //             $('.five-day-goes-here').append(UVIndex)
+    //         },
+    //     })
+    // };
+
+    function show(data) {
+        return "<h1>" + data.name + "<img class='image is-48x48' src= https://openweathermap.org/img/w/" + data.weather[0].icon + ".png>" + "</h1>" +
+            "<p>Temperature: " + data.main.temp + "&deg;F</p>" +
+            "<p>Humidity: " + data.main.humidity + "%</p>" +
+            "<p>Wind Speed: " + data.wind.speed + " MPH</p>"
+    }
 
 });
 
 
-    
-   
+
+
 
